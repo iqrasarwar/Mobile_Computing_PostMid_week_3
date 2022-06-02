d41ec59468e77933bc49a6131c5e81c02d8775fd
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-06-02 09:23:15 +0500

    toast cont

--- app/src/main/java/com/example/lview/Toast.java
+++ app/src/main/java/com/example/lview/Toast.java
@@ -3,24 +3,36 @@ package com.example.lview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.widget.Button;
 
 public class Toast extends AppCompatActivity {
     Button btoast;
+    Button alert;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toast);
         btoast = findViewById(R.id.button);
+        alert = findViewById(R.id.button2);
         btoast.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        LayoutInflater layoutInflater = getLayoutInflater();
+                        View t = layoutInflater.inflate(R.layout.customized_toast,null);
+
                         //inflator
                         //seting and displaying of toast
                     }
                 }
         );
+        alert.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //alert builder inserted here
+            }
+        });
     }
 }
