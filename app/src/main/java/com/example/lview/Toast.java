a37e1e8c28c4a1aa3d72b356aa44f99af7f3b9c3
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-06-02 08:54:37 +0500

    toast strcture added

--- app/src/main/java/com/example/lview/Toast.java
+++ app/src/main/java/com/example/lview/Toast.java
@@ -3,12 +3,24 @@ package com.example.lview;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Toast extends AppCompatActivity {
-
+    Button btoast;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_toast);
+        btoast = findViewById(R.id.button);
+        btoast.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        //inflator
+                        //seting and displaying of toast
+                    }
+                }
+        );
     }
 }
