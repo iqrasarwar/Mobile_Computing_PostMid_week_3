773c3b48e7e42123c97987442ac0c1076930ff70
iqrasarwar <71145709+iqrasarwar@users.noreply.github.com>
2022-06-04 22:56:42 +0500

    multiple choice colors addded to alert

--- app/src/main/java/com/example/lview/myToast.java
+++ app/src/main/java/com/example/lview/myToast.java
@@ -12,10 +12,15 @@ import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.ArrayList;
+
 public class myToast extends AppCompatActivity {
     Button btoast;
-    String [] Colors = {"Red", "Green", "Blue"};
+    String[] Colors = {"Red", "Green", "Blue"};
     Button alert;
+    ArrayList<Integer> selectedItems = new ArrayList(); // Where we track the selected items
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -27,24 +32,60 @@ public class myToast extends AppCompatActivity {
                     @Override
                     public void onClick(View view) {
                         LayoutInflater layoutInflater = getLayoutInflater();
-                        View t = layoutInflater.inflate(R.layout.customized_toast,null);
-                        ImageView imageView=t.findViewById(R.id.imageView2);
+                        View t = layoutInflater.inflate(R.layout.customized_toast, null);
+                        ImageView imageView = t.findViewById(R.id.imageView2);
                         imageView.setImageResource(R.drawable.ic_launcher_background);
-                        TextView myMessage= t.findViewById(R.id.textView4);
+                        TextView myMessage = t.findViewById(R.id.textView4);
                         myMessage.setText("My Custom Toast");
                         Context context = getApplicationContext();
                         Toast.makeText(context, "INSERT TEXT HERE", Toast.LENGTH_LONG).show();
-                        Toast myToast=new Toast(getApplicationContext());
+                        Toast myToast = new Toast(getApplicationContext());
                         myToast.setDuration(Toast.LENGTH_LONG);
                         myToast.setView(t);
                         myToast.show();
                     }
                 }
         );
+
         alert.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                /*AlertDialog.Builder builder = new AlertDialog.Builder(myToast.this);
+                AlertDialog.Builder builder = new AlertDialog.Builder(myToast.this);
+                builder.setTitle("Select options").setMultiChoiceItems(Colors, null,
+                        new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which, boolean isChecked) {
+                                if (isChecked) {
+                                    selectedItems.add(which);
+                                } else if (selectedItems.contains(which)) {
+                                    selectedItems.remove(Integer.valueOf(which));
+                                }
+                            }
+                        }).setCancelable(false)
+                        .setPositiveButton("Yes", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                String msg = "";
+                                for (int i = 0; i < selectedItems.size(); i++) {
+                                    msg = msg + "\n" + (i + 1) + " : " + Colors[selectedItems.get(i)];
+                                }
+                                Toast.makeText(getApplicationContext(), "Total " + selectedItems.size() + " Items Selected.\n" + msg,
+                                        Toast.LENGTH_SHORT).show();
+                            }
+                        })
+                        .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which) {
+                                Toast.makeText(myToast.this, "No Option Selected", Toast.LENGTH_SHORT).show();
+                            }
+                        });
+                AlertDialog dialog = builder.create();
+                dialog.show();
+            }
+        });
+    }
+}
+/*AlertDialog.Builder builder = new AlertDialog.Builder(myToast.this);
                 builder.setMessage("Message you want to show");
                 builder.setTitle("Alert !");
                 builder.setCancelable(false);
@@ -91,7 +132,7 @@ public class myToast extends AppCompatActivity {
                                         Toast.LENGTH_LONG).show();
                             }
                         }).show();*/
-                AlertDialog.Builder builder = new AlertDialog.Builder(myToast.this);
+                /*AlertDialog.Builder builder = new AlertDialog.Builder(myToast.this);
                 builder.setTitle("SetColor")
                         .setItems(Colors, new DialogInterface.OnClickListener() {
                             public void onClick(DialogInterface dialog, int which) {
@@ -99,9 +140,4 @@ public class myToast extends AppCompatActivity {
                             }
                         });
                 AlertDialog dialog = builder.create();
-                dialog.show();
-
-            }
-        });
-    }
-}
+                dialog.show();*/
